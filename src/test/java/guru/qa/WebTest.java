203a61e36e9511f7734c232f54308b880a9144ca
lebedevich-es <lebedevich.es@gmail.com>
2023-05-28 15:33:58 +0200

    updated WebTest class

--- src/test/java/guru/qa/WebTest.java
+++ src/test/java/guru/qa/WebTest.java
@@ -9,11 +9,9 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class WebTest extends TestBase {
 
-    static WebDriverConfig config = ConfigFactory.create(WebDriverConfig.class, System.getProperties());
-
     @Test
     void githubTest() {
-        open(config.getBaseUrl());
+        open("");
         $(".header-search-input").shouldBe(Condition.visible);
     }
 }
