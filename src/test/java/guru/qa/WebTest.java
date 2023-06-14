a83996dfa21843cabd0fc1a1f704810faa39a70d
lebedevich-es <lebedevich.es@gmail.com>
2023-06-14 21:15:15 +0200

    added README

--- src/test/java/guru/qa/WebTest.java
+++ src/test/java/guru/qa/WebTest.java
@@ -1,11 +1,10 @@
 package guru.qa;
 
 import com.codeborne.selenide.Condition;
-import guru.qa.config.WebDriverConfig;
-import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class WebTest extends TestBase {
 
